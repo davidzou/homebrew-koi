352165dd0c3ea414140adf943b591d861ddd8d6b
zouzhangwei <zouzhangwei@hujiang.com>
2018-03-02 16:40:27 +0800

    update sha256

--- Formula/koi.rb
+++ Formula/koi.rb
@@ -2,7 +2,7 @@ class Koi < Formula
   homepage ''
   url 'https://github.com/davidzou/koi/blob/master/tarball/1.0.0/koi-1.0.0.tar?raw=true'
   desc 'A command to create gradle plugin faster'
-  sha256 'eb7dd882cf3bd25c2d4510034129dbb21421e6b86ee3d2fc69918d8c7a19cd8f'
+  sha256 '9e9c5c8cc0650728f1ed521c2ff9021b99cb4068a60e3aae39d6134d79c17bdb'
   version '1.0.0'
 
   def install

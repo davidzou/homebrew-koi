a1323d0d8aaa6fae21d443d6179aabab09b28300
zouzhangwei <zouzhangwei@hujiang.com>
2018-04-27 22:52:20 +0800

    upadte v1.0.2

--- Formula/koi.rb
+++ Formula/koi.rb
@@ -1,9 +1,9 @@
 class Koi < Formula
   homepage ''
-  url 'https://github.com/davidzou/koi/blob/master/tarball/1.0.1/koi-1.0.1.tar?raw=true'
+  url 'https://github.com/davidzou/koi/blob/master/tarball/1.0.2/koi-1.0.2.tar?raw=true'
   desc 'A command to create gradle plugin faster'
-  sha256 '4e92e4168ac45acda08c01999ab46a79aa146e933801a1fd9d5195e254414cdb'
-  version '1.0.1'
+  sha256 'aa43a0110a0941ed34be045e1b585441568b355fa2f9330efcee6c1889479868'
+  version '1.0.2'
 
   def install
     bin.install "koi"
